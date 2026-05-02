c2a6fd86ccb7b6840ec10c1588d8650d2cca454d
Oleksii Panasiuk <alexey.panasiuk.job@gmail.com>
2026-05-02 15:16:47 +0200

    work on comments

--- lesson-5/modules/ecr/ecr.tf
+++ lesson-5/modules/ecr/ecr.tf
@@ -4,4 +4,31 @@ resource "aws_ecr_repository" "repo" {
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
